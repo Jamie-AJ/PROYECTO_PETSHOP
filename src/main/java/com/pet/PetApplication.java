fb47633472aef28ba3335fe41a034000298a1c52
jdelcarpio <jdelcarpio23@gmail.com>
2022-05-26 18:22:46 -0500

    Primer Commit

---
+++ src/main/java/com/pet/PetApplication.java
@@ -0,0 +1,14 @@
+package com.pet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetApplication.class, args);
+		System.out.println("Proyecto Cargado");
+	}
+
+}
