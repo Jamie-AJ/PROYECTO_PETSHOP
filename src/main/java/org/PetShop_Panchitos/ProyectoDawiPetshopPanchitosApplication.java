f55f4fd4fa5ce86213c0d2c0f66572ca5d5e663e
Jeimy Apolaya <jfaj-04@hotmail.com>
2022-05-26 19:03:35 -0500

    Inicio de Proyecto Pet Shop Panchitos

---
+++ src/main/java/org/PetShop_Panchitos/ProyectoDawiPetshopPanchitosApplication.java
@@ -0,0 +1,13 @@
+package org.PetShop_Panchitos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoDawiPetshopPanchitosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoDawiPetshopPanchitosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/PetShop_Panchitos/ProyectoDawiPetshopPanchitosApplicationTests.java
@@ -0,0 +1,13 @@
+package org.PetShop_Panchitos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoDawiPetshopPanchitosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
